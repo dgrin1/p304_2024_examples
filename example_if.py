6a9272c2c432cb23bfd4dab554b8b511765e4e4d
Dan Grin <dgrin@dgrin-22.fios-router.home>
2024-01-25 05:10:11 -0500

    cleaned house

--- example_if.py
+++
@@ -1,9 +0,0 @@
-
-whole=int(input("Choose a integer less than 7:"))
-
-if whole > 7:
-	print(whole,"is larger than 7. You chose badly. I will fix that for you.")
-	whole=5
-	print(whole,"is smaller than 7. Better choice! Je Je Je.")
-
-
--- example_while.py
+++
@@ -1,6 +0,0 @@
-from __future__ import division,print_function
-
-trial=int(input("Choose a multiple of 7:"))
-
-while trial%7 !=0 :
-	trial=int(input("That has a remainder. Try again! Choose a multiple of 7:"))
