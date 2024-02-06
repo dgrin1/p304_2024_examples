e62099b19e7287547cd269fb187a2d4478a840d0
Dan Grin <dgrin@dgrin-22.local>
2024-02-06 10:30:33 -0500

    if examples

---
+++ evenodd_thirdchoice.py
@@ -0,0 +1,15 @@
+
+whole=float(input("Choose a number:"))
+
+#Compute remainder
+r=(whole)%2
+
+
+
+if r==0.0:
+	print(whole,"is even.")
+elif(r==1.0):
+	print(whole,"is odd.")
+else:
+	print(whole,"is real, you silly goose.")
+
