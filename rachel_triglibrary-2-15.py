c9a77f1b740502d197ffe318b09cbe05aecae04b
Rachel McQueen <rmcqueen@haverford.edu>
2024-02-15 10:14:14 -0500

    class activity

---
+++ rachel_triglibrary-2-15.py
@@ -0,0 +1,35 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+matplotlib.rcParams['backend']='TkAgg'
+matplotlib.rcParams['text.usetex']='True'
+matplotlib.rcParams['font.family']='serif'
+matplotlib.rcParams['font.serif']='Times New Roman'
+#plt.ion only works in interactive mode
+plt.ion()
+
+x=np.arange(0, 2*np.pi, 100)
+ySIN = np.sin(x)
+yCOS = np.cos(x)
+yTAN = np.tan(x)
+yASIN = np.arcsin(x)
+yACOS = np.arccos(x)
+yATAN = np.arctan(x)
+
+plt.plot(x, ySIN)
+plt.show()
+
+plt.plot(x, yCOS)
+plt.show()
+
+plt.plot(x, yTAN)
+plt.show()
+
+plt.plot(x, yASIN)
+plt.show()
+
+plt.plot(x, yACOS)
+plt.show()
+
+plt.plot(x, yATAN)
+plt.show()
